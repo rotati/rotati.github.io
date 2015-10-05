d45029021673fce2b41649a0615061058b94a7c1
Pirun SENG <sengpirun.rain@gmail.com>
2015-10-05 14:46:14 +0700

    fix spec to pass for this update

--- spec/teampage_spec.rb
+++ spec/teampage_spec.rb
@@ -7,8 +7,8 @@ describe "team page" do
     {name: 'Pirun', image_filename: 'pirun.png', link: 'https://www.linkedin.com/profile/edit?locale=en_US&trk=profile-preview'},
     {name: 'Siya', image_filename: 'siya.jpg', link: 'https://www.linkedin.com/profile/view?id=AAkAABR3sVcBGWv8URudmAujytZcJYSVwL5FWuE&authType=NAME_SEARCH&authToken=aOS8&locale=en_US&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CclickedEntityId%3A343388503%2CauthType%3ANAME_SEARCH%2Cidx%3A1-1-1%2CtarId%3A1442802513294%2Ctas%3Asiya%20ny'},
     {name: 'Phanith', image_filename: 'phanith.jpg', link: 'https://twitter.com/Mphanith'},
-    {name: 'Victory', image_filename: '/images/anonuser.jpg', link: 'https://www.linkedin.com/in/somethvictory'},
-    {name: 'Sinal', image_filename: '/images/anonuser.jpg', link: 'https://www.linkedin.com/in/meassinal'},
+    {name: 'Victory', image_filename: 'victory.jpg', link: 'https://www.linkedin.com/in/somethvictory'},
+    {name: 'Sinal', image_filename: 'sinal.jpg', link: 'https://www.linkedin.com/in/meassinal'},
     {name: 'Samnang', image_filename: 'samnang.jpg', link: 'https://www.linkedin.com/profile/view?id=AAkAAAG77P8B1DnkRG5HCXcZo7f5ZAR36ereH1U&authType=NAME_SEARCH&authToken=8Ffo&locale=en_US&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CclickedEntityId%3A29093119%2CauthType%3ANAME_SEARCH%2Cidx%3A1-4-4%2CtarId%3A1442216698377%2Ctas%3Asamnang'},
 
 
